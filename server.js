bbc9f469f26575f4d486668c1186b8338e92245c
aesa <adrianoenrico1@gmail.com>
2019-09-12 21:00:54 +0100

    final commit of first version

--- server.js
+++ server.js
@@ -4,3 +4,81 @@ const app = express();
 const bodyParser = require("body-parser");
 const request = require("request");
 
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+app.listen(port = 5000, function(){
+    console.log("Server started on port " + port);
+});
+
+
+app.get("/signup", function(req,res){
+    res.sendFile(__dirname + "/signup.html");
+});
+app.get("/success", function(req,res){
+    res.sendFile(__dirname + "/success.html");
+});
+app.get("/failure", function(req,res){
+    res.sendFile(__dirname + "/failure.html");
+});
+// app.get("/signup.css",function(req,res){
+//     res.sendFile(__dirname + "/signup.css");
+// });
+app.post("/failure",function (req,res){
+    res.redirect("/signup");
+  });
+
+app.post("/signup", function(req,res){
+    var user = {
+        firstName : req.body.firstName,
+        lastName : req.body.lastName,
+        email : req.body.email
+    };    
+    var data = {
+        members : [
+            {
+                email_address: user.email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: user.firstName,
+                    LNAME: user.lastName
+                }
+            }
+        ]
+    };
+    var jsonData = JSON.stringify(data);
+    var options = {
+        url: "https://us4.api.mailchimp.com/3.0/lists/df86c16580",
+        method: "POST",
+        headers: {
+           "Authorization" :  "adriano1 e3307ceda92d87f4496b70940899bd9e-us4"
+        },
+        body: jsonData
+    };
+    
+    request(options, function(error,response,body){
+        if(error){
+            console.log(error);
+            res.redirect("/failure");
+            
+        }
+        else{
+            if(response.statusCode == 200){
+                res.redirect("/success");
+            }
+            else{
+                res.redirect("/failure");
+            }
+            console.log(response.statusMessage);
+            
+        }
+    });
+
+    
+   
+    
+});
+// mailchimp api key
+// e3307ceda92d87f4496b70940899bd9e-us4
+// id for the Unational: df86c16580
+
