893dcee1c40e9a40badea68abd383bb4a0b8e2ae
aesa <adrianoenrico1@gmail.com>
2019-09-12 21:19:07 +0100

    changes made so app can be launched to heroku

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const request = require("request");
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
-app.listen(port = 5000, function(){
+app.listen(process.env.PORT || 5000, function(){
     console.log("Server started on port " + port);
 });
 
@@ -21,9 +21,7 @@ app.get("/success", function(req,res){
 app.get("/failure", function(req,res){
     res.sendFile(__dirname + "/failure.html");
 });
-// app.get("/signup.css",function(req,res){
-//     res.sendFile(__dirname + "/signup.css");
-// });
+
 app.post("/failure",function (req,res){
     res.redirect("/signup");
   });
