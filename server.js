01be175bd40b959216ae281c55015d3818caddc3
aesa <adrianoenrico1@gmail.com>
2019-09-13 15:17:12 +0100

    commit before heroku

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
 app.listen(process.env.PORT || 5000, function(){
-    console.log("Server started on port " + port);
+   
 });
 
 
